8fcacef2c79921e659638c6f721040c9a6e28bd5
svaronc <sebastianvaron96@gmail.com>
2024-03-29 00:51:10 -0500

    change styles to the sidebar and navbar

--- frontend/src/components/MainPage/MainPage.jsx
+++ frontend/src/components/MainPage/MainPage.jsx
@@ -1,9 +1,11 @@
 import Sidebar from "../Sidebar/Sidebar"
+import SidePanel from "../Sidepanel/SidePanel"
 
 function MainPage() {
   return (
-    <div className="h-screen">
+    <div className="h-screen flex">
       <Sidebar/>
+      <SidePanel/>
     </div>
   )
 }
