8208b1532a7f7c74bb8297b625cbea188e09f8b7
svaronc <sebastianvaron96@gmail.com>
2024-03-29 00:39:13 -0500

    refactor the sidebar code

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,44 +1,16 @@
-import { MdDashboard,MdEmail, MdOutlineChatBubble, MdLocalPhone,MdContacts,MdOutlineSettings    } from "react-icons/md";
+import { menuItems } from "../../constants/index"
 
 function Sidebar() {
   return (
     <ul className="menu bg-base-200 content-center h-screen w-[100px] border-r-neutral-800 border-double">
-  <li>
-    <a className="tooltip tooltip-right mt-10" data-tip="Dashboard">
-      <MdDashboard className="w-[30px] h-[30px] "/>
-    </a>
-  </li>
-  <li>
-    <a className="tooltip tooltip-right mt-10" data-tip="Emails">
-    <MdEmail className="w-[30px] h-[30px] "/>
-     
-    </a>
-  </li>
-  <li>
-    <a className="tooltip tooltip-right mt-10" data-tip="Chat">
-    <MdOutlineChatBubble className="w-[30px] h-[30px] "/>
-      
-    </a>
-  </li>
-  <li>
-    <a className="tooltip tooltip-right mt-10" data-tip="Calls">
-    <MdLocalPhone className="w-[30px] h-[30px] "/>
-      
-    </a>
-  </li>
-  <li>
-    <a className="tooltip tooltip-right mt-10" data-tip="Calls">
-    <MdContacts className="w-[30px] h-[30px] "/>
-      
-    </a>
-  </li>
-  <li>
-    <a className="tooltip tooltip-right mt-10" data-tip="Calls">
-    <MdOutlineSettings className="w-[30px] h-[30px] "/>
-      
-    </a>
-  </li>
-</ul>
+    {menuItems.map((item, index) => (
+      <li key={index}>
+        <a className="tooltip tooltip-right mt-10" data-tip={item.tooltip}>
+          {item.icon}
+        </a>
+      </li>
+    ))}
+  </ul>
   )
 }
 
---
+++ frontend/src/constants/index.jsx
@@ -0,0 +1,12 @@
+import { MdDashboard,MdEmail, MdOutlineChatBubble, MdLocalPhone,MdContacts,MdOutlineSettings    } from "react-icons/md";
+
+
+export const menuItems = [
+  { icon: <MdDashboard className="w-[30px] h-[30px] "/>, tooltip: 'Dashboard' },
+  { icon: <MdEmail className="w-[30px] h-[30px] "/>, tooltip: 'Emails' },
+  { icon: <MdOutlineChatBubble className="w-[30px] h-[30px] "/>, tooltip: 'Chat' },
+  { icon: <MdLocalPhone className="w-[30px] h-[30px] "/>, tooltip: 'Calls' },
+  { icon: <MdContacts className="w-[30px] h-[30px] "/>, tooltip: 'Contacts' },
+
+  { icon: <MdOutlineSettings className="w-[30px] h-[30px] "/>, tooltip: 'Settings' },
+];
