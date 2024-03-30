908b7ca28df7fd81f233f767fe76a869e8ad1950
Gloria Lim <glorialimartt@gmail.com>
2024-03-30 12:15:33 -0400

    Modify class in component

--- frontend/src/components/TicketManager/TicketManager.jsx
+++ frontend/src/components/TicketManager/TicketManager.jsx
@@ -23,40 +23,40 @@ function TicketManager() {
     <section className="flex flex-col w-full">
       <TicketManagerNav />
 
-      <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Request
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Customer
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Ticket ID
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Status
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Created
               </th>
             </tr>
           </thead>
           <tbody>
               {requests.map(request => (
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-50 rounded cursor-pointer">
+                <tr key={request.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-50 rounded cursor-pointer">
                   <th
                     scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
                     {request.title}
                   </th>
-                  <td class="px-6 py-4">{request.customer_name}</td>
-                  <td class="px-6 py-4">{request.id}</td>
-                  <td class="px-6 py-4">Open</td>
-                  <td class="px-6 py-4">{request.created_at}</td>
+                  <td className="px-6 py-4">{request.customer_name}</td>
+                  <td className="px-6 py-4">{request.id}</td>
+                  <td className="px-6 py-4">Open</td>
+                  <td className="px-6 py-4">{request.created_at}</td>
                 </tr>
               ))}
           </tbody>
