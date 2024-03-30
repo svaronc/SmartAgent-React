9a7d4a5507a9480e59a0a8c95c2380e09b58a1bb
svaronc <sebastianvaron96@gmail.com>
2024-03-30 18:51:18 -0500

    basic setting to action mailer to respond the requests

--- backend/app/mailers/application_mailer.rb
+++ backend/app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "smartagents3@gmail.com"
   layout "mailer"
+
+  def ticket_response(ticket, response)
+    @ticket = ticket
+    @response = response
+    mail(to: @ticket.request.from_email, subject: "Ticket response", body: @response)
+  end
 end
