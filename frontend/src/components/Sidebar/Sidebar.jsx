0b2e02c04a8fe0cfc21b37112ae584f0fe7a6c6b
Gloria Lim <glorialimartt@gmail.com>
2024-03-29 20:11:27 -0400

    Reduce Sidebar width

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import { menuItems } from "../../constants/index"
 
 function Sidebar() {
   return (
-    <ul className="menu bg-base-200 content-center h-screen w-[100px] border-r-8 border-double">
+    <ul className="menu bg-base-200 content-center h-screen w-[75px] border-r-8 border-double">
     {menuItems.map((item, index) => (
       <li key={index}>
         <a className="tooltip tooltip-right mt-10" data-tip={item.tooltip}>
