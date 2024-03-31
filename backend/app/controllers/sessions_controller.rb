0461aa5cf201dc3e8f7cd4e317edeef9314c1efd
svaronc <sebastianvaron96@gmail.com>
2024-03-31 15:35:13 -0500

    change routes for login and logout and change the seeds

---
+++ backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  # POST /login
+  def create
+    @agent = Agent.find_by(email: params[:username])
+    if @agent && @agent.authenticate(params[:session][:password])
+      session[:agent_id] = @agent.id
+      render json: { logged_in: true }, status: :created
+    else
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
+    end
+  end
+
+  # DELETE /logout
+  def destroy
+    session[:agent_id] = nil
+    render json: { logged_in: false }, status: :ok
+  end
+end
--- backend/app/mailers/application_mailer.rb
+++ backend/app/mailers/application_mailer.rb
@@ -1,10 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "smartagents3@gmail.com"
-  layout "mailer"
+  default from: 'smartagents3@gmail.com'
+  layout 'mailer'
 
   def ticket_response(ticket, response)
     @ticket = ticket
     @response = response
-    mail(to: @ticket.request.from_email, subject: "Ticket response", body: @response)
+    mail(to: @ticket.request.from_email, subject: 'Ticket response', body: @response)
   end
 end
