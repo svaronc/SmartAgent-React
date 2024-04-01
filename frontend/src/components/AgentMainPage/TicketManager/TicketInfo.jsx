f0b6d7227cdd7885c3f568511f359dc63b8d59fe
Gloria Lim <glorialimartt@gmail.com>
2024-03-31 20:40:45 -0400

    Add icons to action buttons

--- frontend/src/components/AgentMainPage/TicketManager/TicketInfo.jsx
+++ frontend/src/components/AgentMainPage/TicketManager/TicketInfo.jsx
@@ -1,43 +1,81 @@
 import { useAppContext } from "../../../context/AppContext";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { MdDelete } from "react-icons/md";
+import { LuArrowLeftRight } from "react-icons/lu";
+import { CgCheckO } from "react-icons/cg";
+import { FaReply } from "react-icons/fa6";
 
-function TicketInfo () {
+function TicketInfo() {
   const { state } = useAppContext();
   const request_id = state.viewTicketId;
   const [request, setRequest] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/api/v1/requests/${request_id}`, {
-      headers: {
-        'Accept': 'application/json'
-      }
-    })
-      .then(response => {
-        setRequest(response.data)
+    axios
+      .get(`http://localhost:3000/api/v1/requests/${request_id}`, {
+        headers: {
+          Accept: "application/json",
+        },
       })
-      .catch(error => {
-        console.error('Error fetching requests', error);
+      .then((response) => {
+        setRequest(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching requests", error);
       });
   }, []);
 
   return (
     <section className="flex-column h-full m-4">
-      <h1>Ticket Info view ticketID: {state.viewTicketId}</h1>
-      <h1 className="text-4xl font-bold">{request.title}</h1>
-      <p>{request.body}</p>
+      <h1 className="text-4xl font-bold mb-4">{request.title}</h1>
+      {/* <h1>Ticket Info view ticketID: {state.viewTicketId}</h1> */}
+      <div className="flex-grow bg-base-100 border-2 border h-1/2 p-4">
+        <p>{request.body}</p>
+      </div>
 
       <div className="justify-end relative bottom-0">
         <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
-          <li><a>Reply</a></li>
-          <li><a>Transfer</a></li>
-          <li><a>Resolve</a></li>
-          <li><a>Close</a></li>
-          <li><a>Delete</a></li>
+          <li>
+            <button className="flex items-center gap-2">
+              <FaReply 
+                size="1.5rem"
+                onClick={() => console.log("reply")}
+              />
+              Reply
+            </button>
+          </li>
+          <li>
+            <button className="flex items-center gap-2">
+              <LuArrowLeftRight
+                size="1.5rem"
+                onClick={() => console.log("transfer")}
+              />
+              Transfer
+            </button>
+          </li>
+          <li>
+            <button className="flex items-center gap-2">
+              <CgCheckO 
+                size="1.5rem" 
+                onClick={() => console.log("resolve")} 
+              />
+              Resolve
+            </button>
+          </li>
+          <li>
+            <button className="flex items-center gap-2">
+              <MdDelete 
+                size="1.5rem" 
+                onClick={() => console.log("delete")} 
+              />
+              Delete
+            </button>
+          </li>
         </ul>
       </div>
     </section>
-  )
-};
+  );
+}
 
 export default TicketInfo;
