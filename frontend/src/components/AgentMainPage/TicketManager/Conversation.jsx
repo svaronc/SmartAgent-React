2b6383ae68f141bff2e0e9f0174acb68572a9015
Gloria Lim <glorialimartt@gmail.com>
2024-04-04 08:24:36 -0400

    Refactor Conversation component. Update logic for rendering from/to email details

--- frontend/src/components/AgentMainPage/TicketManager/Conversation.jsx
+++ frontend/src/components/AgentMainPage/TicketManager/Conversation.jsx
@@ -1,23 +1,37 @@
-function Conversation({ 
-  customer_name, 
-  from_customer, 
-  customer_email, 
-  title, 
-  body 
+function Conversation({
+  customer_name,
+  from_customer,
+  customer_email,
+  title,
+  body,
 }) {
   return (
     <div className="mb-10 border p-5">
       <div className="mb-4 text-gray-500">
-        <p>From: {from_customer ? `${customer_name} <${customer_email}>` : `SmartAgent <smartagents3@gmail.com>`}</p>
-        <p>To: smartagents3@gmail.com</p>
+        {from_customer ? (
+          <div>
+            {/* From customer email details */}
+            <p>
+              From: {customer_name} &lt;{customer_email}&gt;
+            </p>
+            <p>To: SmartAgent &lt;smartagents3@gmail.com&gt;</p>
+          </div>
+        ) : (
+          <div>
+            {/* To customer email details */}
+            <p>From: SmartAgent &lt;smartagents3@gmail.com&gt;</p>
+            <p>
+              To: {customer_name} &lt;{customer_email}&gt;
+            </p>
+          </div>
+        )}
         <p>Subject: {title} </p>
         <div className="flex-grow border-t border-gray-700 mt-4"></div>
       </div>
       <p className="text-2xl"></p>
       <div dangerouslySetInnerHTML={{ __html: body }} />
-      {/* {body} */}
     </div>
-  )
-};
+  );
+}
 
 export default Conversation;
