8267da64f5bab97495efe57099ce3f857fc064bc
Gloria Lim <glorialimartt@gmail.com>
2024-04-04 00:49:01 -0400

    Update TicketManagerNav display during TicketInfo view

--- frontend/src/components/AgentMainPage/TicketManager/TicketManagerNav.jsx
+++ frontend/src/components/AgentMainPage/TicketManager/TicketManagerNav.jsx
@@ -2,17 +2,22 @@ import { useAppContext } from "../../../context/AppContext";
 
 function TicketManagerNav() {
   const { state } = useAppContext();
-
+  console.log(state)
   return (
     <nav>
-      <section className="flex content-start navbar bg-base-100 shadow-md pl-5 w-full">
-        <div className="flex-col items-start">
-          <h1 className="text-2xl font-bold mb-4">{state.ticketManagerView}</h1>
-          <div className="mb-4">       
-            <input type="search" name="search" id="search" placeholder=" Search" className="bg-slate-100"/>
-          </div>
-        </div>
-      </section>
+        {state.ticketInboxView ? 
+          <section className="flex content-start navbar bg-base-100 shadow-md pl-5 w-full">
+            <div className="flex-col items-start">
+              <h1 className="text-2xl font-bold mb-4">{state.ticketManagerView}</h1>
+              <div className="mb-4">       
+                <input type="search" name="search" id="search" placeholder=" Search" className="bg-slate-100"/>
+              </div>
+            </div> 
+          </section> :
+          <>
+          {/* <h1 className="text-2xl font-bold mb-4">Ticket Assigned to: </h1> */}
+          </>
+        }
     </nav>
   )
 }
