ff4de5a99d1cc1fc61c124030478f5052accdf91
Gloria Lim <glorialimartt@gmail.com>
2024-04-04 20:58:46 -0400

    Create hook to fetch all inbox tickets

---
+++ frontend/src/hooks/inbox/useFetchInboxAllTickets.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { ACTIONS } from "../../context/AppContext";
+import axios from "axios";
+
+const useFetchInboxAllTickets = (API_URL, dispatch) => {
+  useEffect(() => {
+    axios
+      .get(API_URL, {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then((res) => {
+        const inboxAllTickets = res.data;
+        dispatch({
+          type: ACTIONS.GET_INBOX_ALL_TICKETS,
+          payload: inboxAllTickets,
+        });
+      });
+  }, []);
+};
+
+export default useFetchInboxAllTickets;
