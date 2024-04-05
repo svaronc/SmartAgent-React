98f5c2732a32e71885e8bfe4e5d97f1e170f5e4b
Gloria Lim <glorialimartt@gmail.com>
2024-04-04 20:59:06 -0400

    Create hook to fetch all triage tickets

---
+++ frontend/src/hooks/inbox/useFetchInboxTriageTickets.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { ACTIONS } from "../../context/AppContext";
+import axios from "axios";
+
+const useFetchInboxTriageTickets = (API_URL, dispatch) => {
+  useEffect(() => {
+    axios
+      .get(API_URL, {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        const inboxTriageTickets = res.data.filter(
+          (ticket) => ticket.status_id === 1 && ticket.agent_id === 1
+        );
+        dispatch({
+          type: ACTIONS.GET_INBOX_ALL_TICKETS,
+          payload: inboxTriageTickets,
+        });
+      });
+  }, []);
+};
+
+export default useFetchInboxTriageTickets;
