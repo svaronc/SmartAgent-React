cede8e42085c637db382dd4390b519de47912380
Gloria Lim <glorialimartt@gmail.com>
2024-04-06 12:49:48 -0400

    Format ConversationAuthor component with prettier

--- frontend/src/components/AgentMainPage/TicketManager/ConversationAuthor.jsx
+++ frontend/src/components/AgentMainPage/TicketManager/ConversationAuthor.jsx
@@ -1,5 +1,4 @@
 function ConversationAuthor({ initial, author }) {
-
   return (
     <div className="conversation-author flex flex-row gap-2 font-bold text-white text-2xl mb-1">
       <div className="avatar placeholder">
@@ -10,7 +9,7 @@ function ConversationAuthor({ initial, author }) {
       {author}
       <span className="font-normal"> wrote</span>
     </div>
-  )
+  );
 }
 
 export default ConversationAuthor;
