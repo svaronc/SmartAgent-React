91b81ff64ef83988f1bd0817382a910342899e6e
Gloria Lim <glorialimartt@gmail.com>
2024-04-06 12:49:01 -0400

    Remove comment

--- frontend/src/components/AgentMainPage/TicketManager/Conversation.jsx
+++ frontend/src/components/AgentMainPage/TicketManager/Conversation.jsx
@@ -52,12 +52,12 @@ function Conversation({
         </div>
         <div>{created_at}</div> {/* Need to format datetime */}
       </div>
-      <div className="flex-grow border-t dark:inherit mb-4"></div>{" "}
+      <div className="flex-grow border-t dark:inherit mb-4"></div>
       {/* Divider */}
       <div
         className="dark:text-white"
         dangerouslySetInnerHTML={{ __html: body }}
-      />{" "}
+      />
       {/* Body of conversation */}
     </div>
   );
