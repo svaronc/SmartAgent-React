eaa18292b769459bd32192cc22a24099bd492e48
Gloria Lim <glorialimartt@gmail.com>
2024-04-06 12:39:22 -0400

    Add author tand avatart to Conversation component.

--- frontend/src/components/AgentMainPage/TicketManager/Conversation.jsx
+++ frontend/src/components/AgentMainPage/TicketManager/Conversation.jsx
@@ -6,47 +6,73 @@ function Conversation({
   title,
   body,
 }) {
-  console.log(from_customer)
   const getConversationDivClassName = (from_customer) => {
-    return from_customer 
-      ? "mb-10 border p-5 dark:bg-gray-700" 
+    return from_customer
+      ? "mb-10 border p-5 dark:bg-gray-700"
       : "mb-10 border p-5 bg-gray-100 dark:bg-inherit";
-  }
+  };
 
   const getConversationInfoDivClassName = (from_customer) => {
-    return from_customer 
-    ? "mb-4 text-gray-400" 
-    : "mb-4 text-gray-500";
-  }
+    return from_customer ? "mb-4 text-gray-400" : "mb-4 text-gray-500";
+  };
+
+  const initial = Array.from(customer_name)[0];
 
   return (
     <div className={getConversationDivClassName(from_customer)}>
-      <div className="flex flex-row justify-between">
-      <div className={getConversationInfoDivClassName(from_customer)}>
-        {from_customer ? (
-          <div>
-            {/* From customer email details */}
-            <p>
-              From: {customer_name} &lt;{customer_email}&gt;
-            </p>
-            <p>To: SmartAgent &lt;smartagents3@gmail.com&gt;</p>
-          </div>
-        ) : (
-          <div>
-            <p className="font-bold text-white text-2xl">SmartAgent <span className="font-normal">wrote</span></p>
-            {/* To customer email details */}
-            <p>From: SmartAgent &lt;smartagents3@gmail.com&gt;</p>
-            <p>
-              To: {customer_name} &lt;{customer_email}&gt;
-            </p>
-          </div>
-        )}
+      <div
+        id="conversation-info-heading"
+        className="flex flex-row justify-between"
+      >
+        <div className={getConversationInfoDivClassName(from_customer)}>
+          {from_customer ? (
+            <div>
+              <div className="conversation-author flex flex-row gap-2 font-bold text-white text-2xl mb-1">
+                <div className="avatar placeholder">
+                  <div className="bg-neutral text-neutral-content rounded-full w-8">
+                    <span className="text-xs">{initial}</span>
+                  </div>
+                </div>
+                {customer_name}
+                <span className="font-normal"> wrote</span>
+              </div>
+
+              {/* From customer email details */}
+              <p>
+                From: {customer_name} &lt;{customer_email}&gt;
+              </p>
+              <p>To: SmartAgent &lt;smartagents3@gmail.com&gt;</p>
+            </div>
+          ) : (
+            <div>
+              <div className="conversation-author flex flex-row gap-2 font-bold text-white text-2xl mb-1">
+                <div className="avatar placeholder">
+                  <div className="bg-neutral text-neutral-content rounded-full w-8">
+                    <span className="text-xs">S</span>
+                  </div>
+                </div>
+                SmartAgent
+                <span className="font-normal"> wrote</span>
+              </div>
+
+              {/* To customer email details */}
+              <p>From: SmartAgent &lt;smartagents3@gmail.com&gt;</p>
+              <p>
+                To: {customer_name} &lt;{customer_email}&gt;
+              </p>
+            </div>
+          )}
           <p>Subject: {title} </p>
         </div>
         <div>{created_at}</div> {/* Need to format datetime */}
       </div>
-        <div className="flex-grow border-t dark:inherit mb-4"></div> {/* Divider */}
-      <div className="dark:text-white" dangerouslySetInnerHTML={{ __html: body }} /> {/* Body of conversation */}
+      <div className="flex-grow border-t dark:inherit mb-4"></div>{" "}
+      {/* Divider */}
+      <div
+        className="dark:text-white"
+        dangerouslySetInnerHTML={{ __html: body }}
+      />{" "}
+      {/* Body of conversation */}
     </div>
   );
 }
