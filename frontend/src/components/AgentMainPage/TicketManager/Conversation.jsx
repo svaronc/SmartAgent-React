a3ffb95ffe34fae85e8aba3edcc29d3f3f809e78
Gloria Lim <glorialimartt@gmail.com>
2024-04-06 10:43:59 -0400

    Update conversation background and text color themes based on received vs sent responses

--- frontend/src/components/AgentMainPage/TicketManager/Conversation.jsx
+++ frontend/src/components/AgentMainPage/TicketManager/Conversation.jsx
@@ -5,9 +5,22 @@ function Conversation({
   title,
   body,
 }) {
+  console.log(from_customer)
+  const getConversationDivClassName = (from_customer) => {
+    return from_customer 
+      ? "mb-10 border p-5 dark:bg-gray-700" 
+      : "mb-10 border p-5 bg-gray-100 dark:bg-inherit";
+  }
+
+  const getConversationInfoDivClassName = (from_customer) => {
+    return from_customer 
+    ? "mb-4 text-gray-400" 
+    : "mb-4 text-gray-500";
+  }
+
   return (
-    <div className="mb-10 border p-5">
-      <div className="mb-4 text-gray-500">
+    <div className={getConversationDivClassName(from_customer)}>
+      <div className={getConversationInfoDivClassName(from_customer)}>
         {from_customer ? (
           <div>
             {/* From customer email details */}
@@ -26,10 +39,9 @@ function Conversation({
           </div>
         )}
         <p>Subject: {title} </p>
-        <div className="flex-grow border-t border-gray-700 mt-4"></div>
+        <div className="flex-grow border-t dark:inherit mt-4"></div> {/* Divider */}
       </div>
-      <p className="text-2xl"></p>
-      <div dangerouslySetInnerHTML={{ __html: body }} />
+      <div className="dark:text-white" dangerouslySetInnerHTML={{ __html: body }} /> {/* Body of conversation */}
     </div>
   );
 }
