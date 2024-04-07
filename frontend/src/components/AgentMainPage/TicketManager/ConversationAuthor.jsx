7f1ce7e750ce37f412f4ed07a898dd10d64d6481
Gloria Lim <glorialimartt@gmail.com>
2024-04-07 18:19:43 -0400

    Fix light mode ConversationAuthor text color

--- frontend/src/components/AgentMainPage/TicketManager/ConversationAuthor.jsx
+++ frontend/src/components/AgentMainPage/TicketManager/ConversationAuthor.jsx
@@ -1,6 +1,6 @@
 function ConversationAuthor({ initial, author }) {
   return (
-    <div className="conversation-author flex flex-row gap-2 font-bold text-white text-2xl mb-1">
+    <div className="conversation-author flex flex-row gap-2 font-bold text-gray-700 dark:text-white text-2xl mb-1">
       <div className="avatar placeholder">
         <div className="bg-neutral text-neutral-content rounded-full w-8">
           <span className="text-xs">{initial}</span>
