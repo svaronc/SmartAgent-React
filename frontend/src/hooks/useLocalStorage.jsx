7ef97c989449c6a3e1b91f87285947e4b7dea47d
Gloria Lim <glorialimartt@gmail.com>
2024-04-07 20:10:59 -0400

    Add useLocalStorage hook to update state based on agent info in local storage

---
+++ frontend/src/hooks/useLocalStorage.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { ACTIONS, useAppContext } from "../context/AppContext";
+
+/**
+ * A custom hook for setting agent name and ID in state based on memo data
+ * @function
+ */
+
+const useLocalStorage = () => {
+  const { state, dispatch } = useAppContext();
+
+  // useEffect foe handling agent info stored in local storage
+  useEffect(() => {
+    const localAgentId = window.localStorage.getItem("agent_id")
+    const localAgentFullName = window.localStorage.getItem("full_name")
+    
+    if (localAgentId && localAgentFullName) {
+      // Set Agent data in state
+      dispatch({ type: ACTIONS.SET_AGENT, payload: { agent_id: localAgentId, full_name: localAgentFullName }})
+    }
+  }, [])
+};
+
+export default useLocalStorage;
