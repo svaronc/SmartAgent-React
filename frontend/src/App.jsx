fd79269f94df6a1f673b4d018f8674955d92c896
Gloria Lim <glorialimartt@gmail.com>
2024-04-08 18:26:58 -0400

    Update application to show on full width and height of screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Hero from "./components/Landing/Hero";
 import Login from "./components/Login/Login";
 import Navbar from "./components/AgentMainPage/Navbar";
-import Footer from "./components/Footer/Footer";
 import AgentMainPage from "./components/AgentMainPage/AgentMainPage";
 import { AppProvider } from "./context/AppContext";
 import useApplicationData from "./hooks/useApplicationData";
@@ -27,7 +26,6 @@ function App() {
     <AuthProvider>
       <AppProvider>
         <BrowserRouter>
-          <Navbar />
           <AppRoutes />
         </BrowserRouter>
       </AppProvider>
