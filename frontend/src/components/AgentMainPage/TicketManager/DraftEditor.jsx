873a9334de67c88ee58d53c8d2cc61e941371204
Gloria Lim <glorialimartt@gmail.com>
2024-04-09 02:10:05 -0400

    Update canned message defaultValue. Convert editorState from html to markdown

--- frontend/src/components/AgentMainPage/TicketManager/DraftEditor.jsx
+++ frontend/src/components/AgentMainPage/TicketManager/DraftEditor.jsx
@@ -3,18 +3,22 @@ import ReactQuill from 'react-quill'
 import 'quill/dist/quill.snow.css'
 import DOMPurify from 'dompurify';
 
+// HTML to Markdown converter
+import turndown from "turndown";
 // Import constants
 import { modules, formats } from '../../../constants/draft-editor-format';
 
 function DraftEditor({ customer_name, editorState, setEditorState }) {
-
+  const TurndownService = new turndown();
+  
   const handleProcedureContentChange = (content) => {
     console.log("content---->", content);
     const sanitizedContent = DOMPurify.sanitize(content)
-    setEditorState(sanitizedContent); // Update editorState with the content
+    // Update editorState with the content that is converted to markdown
+    setEditorState(TurndownService.turndown(sanitizedContent));    
   };
 
-  const defaultValue = `<p>Hi ${customer_name}, </p><p><br></p><p><br></p><p>Regards,</p><p><br></p><p><strong>SmartAgent</strong></p><p>Customer Support</p><p>smartagents3@gmail.com</p>`
+  const defaultValue = `<p>Hi ${customer_name}, </p><p><br></p><br><p><br></p><p>Regards,</p><p><br></p><p><strong>SmartAgent</strong></p><p>Customer Support</p><a href="mailto:smartagents3@gmail.com">smartagents3@gmail.com</a>`
 
   return (
     <div>
