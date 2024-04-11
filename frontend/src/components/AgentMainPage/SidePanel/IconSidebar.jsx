91df6ef7251c42c3f475453715607461f7ab4ac8
Gloria Lim <glorialimartt@gmail.com>
2024-04-11 15:07:11 -0400

    Change IconSidebar color

--- frontend/src/components/AgentMainPage/SidePanel/IconSidebar.jsx
+++ frontend/src/components/AgentMainPage/SidePanel/IconSidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 function IconSidebar() {
   return (
-    <ul className="menu bg-base-200 content-center w-[75px] border-r-8 border-double h-full">
+    <ul className="menu bg-base-100 content-center w-[75px] border-r-8 border-double h-full">
     {menuItems.map((item, index) => (
       <li key={index}>
         <Link to={item.path} className="tooltip tooltip-right mt-10" data-tip={item.tooltip}>
