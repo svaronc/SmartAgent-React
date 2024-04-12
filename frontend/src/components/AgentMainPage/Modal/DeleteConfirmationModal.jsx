b61110c0354b67b96282493f8d2379aa80a0ee71
Gloria Lim <glorialimartt@gmail.com>
2024-04-12 10:19:33 -0400

    Add functionality to close modal when clicking on backdrop

--- frontend/src/components/AgentMainPage/Modal/DeleteConfirmationModal.jsx
+++ frontend/src/components/AgentMainPage/Modal/DeleteConfirmationModal.jsx
@@ -7,6 +7,7 @@ function DeleteConfirmationModal({ ticket_id }) {
     <div className="m-0 p-0">
       <input type="checkbox" id="my_modal_7" className="modal-toggle" />
       <div className="modal" role="dialog">
+        <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
         <div className="modal-box pb-1">
           <h3 className="text-lg font-bold dark:text-white">
             This action is reversible!
