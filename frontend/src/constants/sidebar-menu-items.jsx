09b7e34671aadc4e5abb99fa85f329ed10a46d08
Gloria Lim <glorialimartt@gmail.com>
2024-04-12 09:31:22 -0400

    Remove settings icon from IconSidebar

--- frontend/src/constants/sidebar-menu-items.jsx
+++ frontend/src/constants/sidebar-menu-items.jsx
@@ -1,10 +1,8 @@
-import { MdDashboard,MdEmail, MdOutlineChatBubble, MdLocalPhone,MdContacts,MdOutlineSettings    } from "react-icons/md";
-
+import { MdDashboard, MdEmail, MdOutlineChatBubble, MdContacts } from "react-icons/md";
 
 export const menuItems = [
   { icon: <MdDashboard className="w-[30px] h-[30px] "/>, tooltip: 'Dashboard', path: '/dashboard' },
   { icon: <MdEmail className="w-[30px] h-[30px] "/>, tooltip: 'Emails', path: '/main' },
   { icon: <MdOutlineChatBubble className="w-[30px] h-[30px] "/>, tooltip: 'AI Chatbot', path: '/chat' },
   { icon: <MdContacts className="w-[30px] h-[30px] "/>, tooltip: 'Agents', path: '/agents' },
-  { icon: <MdOutlineSettings className="w-[30px] h-[30px] "/>, tooltip: 'Profile', path: '/profile' },
 ];
