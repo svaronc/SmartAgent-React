2e8126af3f07c5934a8d50992aade4e9f2e9ea93
svaronc <sebastianvaron96@gmail.com>
2024-04-13 12:51:33 -0500

    delete notes when deleting tickets

--- backend/app/models/ticket.rb
+++ backend/app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ApplicationRecord
   has_many :conversations, dependent: :destroy
+  has_many :notes, dependent: :destroy
   has_many_attached :attachments
   belongs_to :agent
   belongs_to :status
