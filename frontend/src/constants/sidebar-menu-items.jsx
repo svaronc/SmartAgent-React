004dba8dd3def5aaed7200c4505fc8d7855bd49f
Gloria Lim <glorialimartt@gmail.com>
2024-04-15 20:08:33 -0400

    Update logo in TicketsSidePanel

--- frontend/src/constants/sidebar-menu-items.jsx
+++ frontend/src/constants/sidebar-menu-items.jsx
@@ -1,12 +1,12 @@
 import { MdDashboard, MdEmail, MdOutlineChatBubble } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
-import { GoHomeFill } from "react-icons/go";
+// import { GoHomeFill } from "react-icons/go";
 
 export const menuItems = [
+  // { icon: <GoHomeFill className="w-[30px] h-[30px] "/>, tooltip: 'Welcome Page', path: '/' },
   { icon: <MdDashboard className="w-[30px] h-[30px] "/>, tooltip: 'Dashboard', path: '/dashboard' },
   { icon: <MdEmail className="w-[30px] h-[30px] "/>, tooltip: 'Emails', path: '/main' },
   { icon: <MdOutlineChatBubble className="w-[30px] h-[30px] "/>, tooltip: 'Chat', path: '/chat' },
   { icon: <CgProfile className="w-[30px] h-[30px] "/>, tooltip: 'Profile', path: '/profile' },
-  { icon: <GoHomeFill className="w-[30px] h-[30px] "/>, tooltip: 'Welcome Page', path: '/' },
  
 ];
