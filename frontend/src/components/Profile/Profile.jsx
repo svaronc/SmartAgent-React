6494bb015681ec5414789c1fd883ab6d9d57426d
Gloria Lim <glorialimartt@gmail.com>
2024-04-20 15:40:27 -0400

    Update Profile layout

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -14,7 +14,7 @@ function Profile() {
   const currentAgent = state.agents.find(
     (agent) => agent.id === Number(logAgentId)
   );
-  console.log(currentAgent);
+  console.log("currentAgent", currentAgent);
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
@@ -41,52 +41,54 @@ function Profile() {
     }
   };
   return (
-    <div className="flex flex-col items-center justify-center p-6 bg-white shadow rounded-lg">
-      <img
-        className="w-24 h-24 rounded-full mb-4"
-        src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Profile"
-      />
-      <h1 className="text-xl font-semibold">{currentAgent?.full_name}</h1>
-      <p className="text-gray-600">{currentAgent?.email}</p>
-      <div className="mt-4 w-full px-3">
-        <label
-          htmlFor="password"
-          className="block text-sm font-medium text-gray-700"
+    <div className="h-full flex flex-col items-center justify-center">
+
+      <div className="flex flex-col items-center justify-center w-1/2 p-10 bg-white shadow rounded-lg">
+        <div className="flex justify-center items-center h-24 w-24 mb-3 bg-gray-200 text-gray-700 rounded-full mx-3">
+          <span className="text-4xl">{currentAgent?.full_name.charAt(0)}</span>
+        </div>
+        <h1 className="text-xl font-semibold">{currentAgent?.full_name}</h1>
+        <p className="text-gray-600">{currentAgent?.email}</p>
+        <div className="w-full px-3 p-5 m-5">
+        <h1 className="text-2xl font-semibold mb-5">Reset Password</h1>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            New Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          />
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700 mt-5"
+          >
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={repeatPassword}
+            onChange={handleRepeatPasswordChange}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          />
+        </div>
+        {errorMessage && <p className="text-red-500 mt-3">{errorMessage}</p>}
+        {message && <p className="text-green-500 mt-3">{message}</p>}
+        <button
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg"
+          onClick={handleSubmit}
         >
-          Change Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={handlePasswordChange}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-        />
-        <label
-          htmlFor="password"
-          className="block text-sm font-medium text-gray-700"
-        >
-          Repeat Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={repeatPassword}
-          onChange={handleRepeatPasswordChange}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-        />
+          Submit
+        </button>
       </div>
-      {errorMessage && <p className="text-red-500 mt-3">{errorMessage}</p>}
-      {message && <p className="text-green-500 mt-3">{message}</p>}
-      <button
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg"
-        onClick={handleSubmit}
-      >
-        Submit
-      </button>
     </div>
   );
 }
