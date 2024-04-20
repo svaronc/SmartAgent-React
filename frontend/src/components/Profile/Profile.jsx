8853bc3d7a7a72010bae0b01d15dc48204f86e66
Gloria Lim <glorialimartt@gmail.com>
2024-04-20 16:08:57 -0400

    Improve profile page password reset section

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useAppContext } from "../../context/AppContext";
 import axios from "axios";
 
-
 function Profile() {
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
@@ -31,7 +30,7 @@ function Profile() {
       return;
     } else {
       axios
-        .put(`/api/v1/agents/${currentAgent.id}`, { agent: { password }})
+        .put(`/api/v1/agents/${currentAgent.id}`, { agent: { password } })
         .then((response) => {
           setMessage(response.data.message);
           setPassword("");
@@ -42,15 +41,14 @@ function Profile() {
   };
   return (
     <div className="h-full flex flex-col items-center justify-center">
-
-      <div className="flex flex-col items-center justify-center w-1/2 p-10 bg-white shadow rounded-lg">
-        <div className="flex justify-center items-center h-24 w-24 mb-3 bg-gray-200 text-gray-700 rounded-full mx-3">
+      <div className="flex flex-col dark:text-black dark:bg-gray-300 items-center justify-center w-1/2 p-10 bg-white shadow rounded-lg">
+        <div className="flex justify-center items-center h-24 w-24 mb-3 dark:bg-gray-100 bg-gray-200 text-gray-700 rounded-full mx-3">
           <span className="text-4xl">{currentAgent?.full_name.charAt(0)}</span>
         </div>
         <h1 className="text-xl font-semibold">{currentAgent?.full_name}</h1>
         <p className="text-gray-600">{currentAgent?.email}</p>
         <div className="w-full px-3 p-5 m-5">
-        <h1 className="text-2xl font-semibold mb-5">Reset Password</h1>
+          <h1 className="text-2xl font-semibold mb-5">Reset Password</h1>
           <label
             htmlFor="password"
             className="block text-sm font-medium text-gray-700"
@@ -63,7 +61,7 @@ function Profile() {
             name="password"
             value={password}
             onChange={handlePasswordChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            className="dark:text-white mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
           />
           <label
             htmlFor="password"
@@ -77,11 +75,30 @@ function Profile() {
             name="password"
             value={repeatPassword}
             onChange={handleRepeatPasswordChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            className="dark:text-white mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
           />
+          {
+            message && !errorMessage && (
+              <div role="alert" className="alert alert-success mt-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-current shrink-0 h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>{message}</span>
+              </div>
+            )
+          }
+          {errorMessage && <p className="text-red-500 mt-3 font-bold">{errorMessage}</p>}
         </div>
-        {errorMessage && <p className="text-red-500 mt-3">{errorMessage}</p>}
-        {message && <p className="text-green-500 mt-3">{message}</p>}
         <button
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg"
           onClick={handleSubmit}
