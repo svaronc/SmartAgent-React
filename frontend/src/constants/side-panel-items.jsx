e26b32221316a89637b124a7f80ae214935bd114
Gloria Lim <glorialimartt@gmail.com>
2024-04-21 17:40:46 -0400

    Update side panel filters

--- frontend/src/constants/side-panel-items.jsx
+++ frontend/src/constants/side-panel-items.jsx
@@ -1,6 +1,7 @@
 export const sidePanelItems = [
   { title: "Assigned to Me", count: 'countAssignedToMe' },
   { title: "Triage - Open Tickets", count: 'countTriage' },
+  { title: "All Answered Tickets", count: 'countAnswered' },
+  { title: "All Resolved Tickets", count: 'countClosed' },
   { title: "All Tickets", count: 'countAll' },
-  { title: "Resolved Tickets", count: 'countClosed' },
 ];
