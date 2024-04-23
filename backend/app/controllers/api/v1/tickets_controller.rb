d6c855bb54390c96dd224629e9c8e8b0bc97009f
Gloria Lim <glorialimartt@gmail.com>
2024-04-23 15:45:56 -0400

    Update ticket order

--- backend/app/controllers/api/v1/tickets_controller.rb
+++ backend/app/controllers/api/v1/tickets_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::TicketsController < ApplicationController
   # GET api/v1/tickets
   # GET api/v1/tickets.json
   def index
-    @tickets = Ticket.order(created_at: :desc, title: :asc, status_id: :asc)
+    @tickets = Ticket.order(status_id: :asc, created_at: :desc, title: :asc)
     render json: @tickets.as_json(include: {
                                     conversations: {
                                       methods: :attachments_urls
