34897c0c302b2e99ec8795414321919cec458c5b
Gloria Lim <glorialimartt@gmail.com>
2024-04-25 18:30:20 -0400

    Add mock data

---
+++ frontend/src/mockData/agents.jsx
@@ -0,0 +1,102 @@
+const agents = [
+  {
+    id: 7,
+    email: "oswaldo_cronin@dickens.example",
+    full_name: "Brian Johns",
+    password_digest:
+      "$2a$12$MRYbsUEhw2I7AOX7WLx9O.CUC2WJVYG4/VbXN4wyBW8twNGrf68rO",
+    role_id: null,
+    created_at: "2024-04-23T00:33:54.568Z",
+    updated_at: "2024-04-23T00:33:54.568Z",
+  },
+  {
+    id: 4,
+    email: "carman.schmeler@konopelski-collier.example",
+    full_name: "Carroll Ratke JD",
+    password_digest:
+      "$2a$12$eYYZ30U49ky0gDE/GEnSfeTlIu4dTNnfMQdoCAISSjBNM.Srfk2CC",
+    role_id: null,
+    created_at: "2024-04-23T00:33:53.914Z",
+    updated_at: "2024-04-23T00:33:53.914Z",
+  },
+  {
+    id: 10,
+    email: "carlo@morissette-bauch.test",
+    full_name: "Dr. Pa Mueller",
+    password_digest:
+      "$2a$12$/3HkxhNktv2nutzmWrIs0OG/Z8T//C.1AS59f4hsOBFwKoyzmJ3iG",
+    role_id: null,
+    created_at: "2024-04-23T00:33:55.219Z",
+    updated_at: "2024-04-23T00:33:55.219Z",
+  },
+  {
+    id: 3,
+    email: "glorialimartt@gmail.com",
+    full_name: "Gloria Lim",
+    password_digest:
+      "$2a$12$XHSKwwLI2yYXFA1fl/8rguBMohoj1BSxtkOO7nQ6bup59Ok5Q.e4i",
+    role_id: null,
+    created_at: "2024-04-23T00:33:53.140Z",
+    updated_at: "2024-04-23T00:33:53.140Z",
+  },
+  {
+    id: 8,
+    email: "luke@blanda-christiansen.test",
+    full_name: "Loida Grant Sr.",
+    password_digest:
+      "$2a$12$RMLhkPc/LO08E8PaoSNsa.DeFA0F4vgacj5WJNJBXNXFjUIBDE6W.",
+    role_id: null,
+    created_at: "2024-04-23T00:33:54.790Z",
+    updated_at: "2024-04-23T00:33:54.790Z",
+  },
+  {
+    id: 9,
+    email: "kelvin_jones@barton-durgan.example",
+    full_name: "Miguel Sanford",
+    password_digest:
+      "$2a$12$mBfQCpiAEadAM7sAOsmpv.mGe2d5/LKoODEtBwr2cWC7afiul9CjO",
+    role_id: null,
+    created_at: "2024-04-23T00:33:55.005Z",
+    updated_at: "2024-04-23T00:33:55.005Z",
+  },
+  {
+    id: 6,
+    email: "albertina_wiza@beatty-kling.test",
+    full_name: "Ms. Colin Monahan",
+    password_digest:
+      "$2a$12$GgkkYC.etHUwIbXPkUBn.OQY9h.GGcEhqvYGdY3DYUUWR3jIu6YXa",
+    role_id: null,
+    created_at: "2024-04-23T00:33:54.352Z",
+    updated_at: "2024-04-23T00:33:54.352Z",
+  },
+  {
+    id: 5,
+    email: "donny.luettgen@fahey-mccullough.example",
+    full_name: "Norris Erdman",
+    password_digest:
+      "$2a$12$fSjt3apBuuqwkwjCUJ9a4O5sEG7184gT.FeVy4ibMDUZzd55fEFl.",
+    role_id: null,
+    created_at: "2024-04-23T00:33:54.131Z",
+    updated_at: "2024-04-23T00:33:54.131Z",
+  },
+  {
+    id: 2,
+    email: "sebastianvaron96@gmail.com",
+    full_name: "Sebastian varon",
+    password_digest:
+      "$2a$12$iApkqrRSh6o2fGXPTqgj9./4Dj01PHmj4EOtSbqXT16Xl/oTiZ/2C",
+    role_id: null,
+    created_at: "2024-04-23T00:33:52.909Z",
+    updated_at: "2024-04-23T00:33:52.909Z",
+  },
+  {
+    id: 1,
+    email: "triage@gmail.com",
+    full_name: "Triage",
+    password_digest:
+      "$2a$12$KILL6at79K2LYW.VyotsBeXY9mBIFSSomupHlLMznQsEAFEVXmGdK",
+    role_id: null,
+    created_at: "2024-04-23T00:33:52.669Z",
+    updated_at: "2024-04-23T00:33:52.669Z",
+  },
+];
---
+++ frontend/src/mockData/statuses.jsx
@@ -0,0 +1,20 @@
+const statuses = [
+  {
+      "id": 1,
+      "description": "Open",
+      "created_at": "2024-04-23T00:33:55.236Z",
+      "updated_at": "2024-04-23T00:33:55.236Z"
+  },
+  {
+      "id": 2,
+      "description": "Answered",
+      "created_at": "2024-04-23T00:33:55.246Z",
+      "updated_at": "2024-04-23T00:33:55.246Z"
+  },
+  {
+      "id": 3,
+      "description": "Resolved",
+      "created_at": "2024-04-23T00:33:55.254Z",
+      "updated_at": "2024-04-23T00:33:55.254Z"
+  }
+]
